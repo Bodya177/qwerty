f9ef4a74363b6a15794db22911e491919dd56165
Bodya177 <bodya13211@gmail.com>
2021-11-05 14:42:55 +0300

    new1

---
+++ qwerty!.cpp
@@ -0,0 +1,26 @@
+#include <iomanip>
+#include<math.h>
+#include<windows.h>
+#include <iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+    float minX, maxX, h, x;
+    double y;
+    cin >> minX >> maxX >> h;
+    x = minX;
+    do 
+    {
+        y = (pow(x, 5) + 3 * tan(pow(x, 2) + 2));
+        (x += h);
+        cout << setw(10) << left << x << setw(4) << y << endl;
+       
+    }
+       while (x <= maxX + h / 2)
+
+
+
+
+}
